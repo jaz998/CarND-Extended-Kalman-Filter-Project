57ce7aeb484ad2e9213921b1b5d2c0b44a58e4c6
jaz998 <jaz998@users.noreply.github.com>
2018-09-01 23:12:30 +1000

    tool.cpp CalculateRMSE done
    Program compiled and run without errors.

--- src/tools.cpp
+++ src/tools.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 #include "tools.h"
+#include <math.h>
 
 using Eigen::VectorXd;
 using Eigen::MatrixXd;
 using std::vector;
+
+using namespace std;
 
 Tools::Tools() {}
 
@@ -15,6 +18,31 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
   TODO:
     * Calculate the RMSE here.
   */
+	VectorXd RMSE(4);
+	RMSE << 0, 0, 0, 0;
+
+	//Perform validation of the input data
+	if (estimations.size() == 0 || ground_truth.size() == 0) {
+		cout << "Input validation failed: either estimations or ground truth has zero size" << endl;
+		return RMSE;
+	}
+	if (estimations.size() != ground_truth.size()) {
+		cout << "Input validation failed: estimations and groud truths have different sizes" << endl;
+		return RMSE;
+	}
+
+	for (unsigned int i; i < estimations.size(); i++) {
+		VectorXd diff = estimations[i] - ground_truth[i];
+		diff = pow(diff.array(), 2);
+		RMSE = diff + RMSE;
+	}
+	RMSE = RMSE / estimations.size();
+	RMSE = sqrt(RMSE.array());
+	return RMSE;
+
+
+
+
 }
 
 MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
